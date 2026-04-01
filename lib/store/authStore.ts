65b43cf9dc9ecb5b4e6bdef4f030b3ece2ac027f
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-04-01 15:56:57 +0200

    Update authStore.ts

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -1,24 +1,25 @@
-import { User } from "@/types/user";
-import { create } from "zustand";
+import { create } from 'zustand';
 
-interface AuthStore {
-  user: User | null;
-  isAuthenticated: boolean;
-  setUser: (newUser: User) => void;
+interface AuthState {
+  user: any | null;
+  isAuthenticated: boolean | null;
+  setUser: (user: any) => void;
   clearIsAuthenticated: () => void;
 }
 
-export const useAuthStore = create<AuthStore>()((setStore) => {
-  return {
-    user: null,
-    isAuthenticated: false,
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: null, 
 
-    setUser: (user) => {
-      setStore({ user: user, isAuthenticated: true });
-    },
+  setUser: (user) =>
+    set({
+      user,
+      isAuthenticated: true,
+    }),
 
-    clearIsAuthenticated: () => {
-      setStore({ user: null, isAuthenticated: false });
-    },
-  };
-});
+  clearIsAuthenticated: () =>
+    set({
+      user: null,
+      isAuthenticated: false,
+    }),
+}));
